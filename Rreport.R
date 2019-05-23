735a8969603b404a61f02a2dc70e79592c966426
Numa Leone Elizalde <numa.leone@ing.austral.edu.ar>
2019-05-23 00:01:37 -0300

    Update Rreport.R

--- Rreport.R
+++ Rreport.R
@@ -1,13 +1,9 @@
-setwd("C:/Users/Numa/Desktop/PredictorAcademico")
 getwd()
 
 library(readxl)
 library(dplyr)
 library(tidyr)
 
-entry19 <- read_excel("C:/Users/Numa/Desktop/PredictorAcademico/INGRESO 2019-ANON.xlsx")
-entry18 <- read_excel("C:/Users/Numa/Desktop/PredictorAcademico/INGRESO 2018-ANON.xlsx")
-entry1819 <- read_excel("C:/Users/Numa/Desktop/PredictorAcademico/INGRESO 2018-2019-ANON.xlsx")
 
 
 names(entry19)
