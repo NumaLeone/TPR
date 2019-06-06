0ee3a50b8dd05a3bedd7f523e23c77a85d9e7243
sofiasdz <sofia.sidanez@ing.austral.edu.ar>
2019-06-06 12:03:24 -0300

    olanda

--- CleanReport.R
+++ CleanReport.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(dplyr)
 library(tidyr)
+library(colortools)
 
 changeErrors <- function(str, tokenArray, fixedArray, booleanFixed = FALSE){
   newArray <- sapply(tokenArray, grepl, str, ignore.case = TRUE, fixed = booleanFixed)
@@ -32,7 +33,8 @@ entry<- rbind(entry18,entry19)
 wrong <- c("NO","BAJA","OBTIENE" ,"BFI N/A","Si","N/A","MO","falta")
 correct <- c("NO","NO","SI","SI","SI","NO","NO",NA)
 entry$OBT.SCHOLARSHIP <- sapply(entry$OBT.SCHOLARSHIP, changeErrors, wrong, correct)
-entry$OBT.SCHOLARSHIP[which(entry$OBT.SCHOLARSHIP!="SI")] <-"NO"
+entry$OBT.SCHOLARSHIP[which(entry$OBT.SCHOLARSHIP != "SI")] <-"NO"
+entry$OBT.SCHOLARSHIP[which(is.na(entry$OBT.SCHOLARSHIP))] <-"NO"
 
 
 wrong <-c("Cuatrimestral","Febrero", "Octubre", "Septiembre","Agosto","Directo","Diciembre")
@@ -45,6 +47,10 @@ entry$CMATH <- sapply(entry$CMATH,changeErrors,wrong,correct)
 entry$RMATH <- sapply(entry$RMATH,changeErrors,wrong,correct)
 entry$CPHYS <- sapply(entry$CPHYS,changeErrors,wrong,correct)
 entry$RPHYS <- sapply(entry$RPHYS,changeErrors,wrong,correct) 
+
+entry$CMATH<- as.numeric(entry$CMATH)
+entry$CPHYS<- as.numeric(entry$CPHYS)
+
 
 wrong <- c("NO","BAJA","OBTIENE" ,"BFI N/A","Si")
 correct <- c("NO","NO","SI","SI","SI")
@@ -59,16 +65,25 @@ correct <- c("Antonio Berni", "Bede's Grammar School", "Cardenal Pironio", "Cole
 entry$SCHOOL <- sapply(entry$SCHOOL, changeErrors, wrong, correct)
 
 barplot(table(entry$CMATH),main= "Notas Curso de Ingreso Matematica", col = rainbow(12))
+pie3D(table(entry$CMATH), explode = 0.1, main="Notas Curso de Ingreso Matematica",labels = c("1","2","3","4","5","6","7","8","9","10"),labelcex = 0.9)
 barplot(table(entry$CPHYS),main= "Notas Curso de Ingreso Fisica", col = rainbow(12))
 
 approvedMath <- which(entry$CMATH > "3")
 approvedPhys <- which(entry$CPHYS >"3")
 approved <- intersect(approvedMath,approvedPhys)
 
+
 approvedRows <- entry[approved,]
 
+carreer<-as.factor(entry$Carreer)
+sex<-as.factor(entry$Sex)
+barplot(table(sex,carreer),beside=TRUE,legend.text=TRUE,col=c("hotpink","cyan"),main="Proporcion De Mujeres y Hombres Por Carrera")
 
 
+library(plotrix)
+beca<-as.factor(entry$OBT.SCHOLARSHIP)
+ingreso<-as.factor(entry$Entry)
+barplot(table(beca,ingreso),legend.text=TRUE,col=c("#3B14AF","#E6399B"),ylab="Cantidad de Alumnos",xlab="Ingreso",main="Beca Por Curso De Ingreso")
 
 #pie()#preguntar bauti sobre como hacer piechart que compare los que aprobaron todas con los que no
 
